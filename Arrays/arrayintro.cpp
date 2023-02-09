d0ffaf71d03e407be220c1dec356a7663190bdc0
thisissamridh <samridhsinghind@gmail.com>
2023-02-09 20:00:08 +0530

    checking gb values

--- Arrays/arrayintro.cpp
+++ Arrays/arrayintro.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 using namespace std;
 
+void initialiseAllWithSameValue(int array[], int k, int value)
+{
+    for (int i = 0; i < k; i++)
+    {
+        array[i] = value;
+    }
+}
+
 int main()
 {
 
-    int number[5];
+    int number[50];
     cout << endl
          << "everything is fine" << endl
          << endl;
+
+    cout << "value at 15 location: " << number[15] << endl;
+
     return 0;
 }
