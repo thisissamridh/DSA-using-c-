7342a77e3150f7e15276243da556c9f2d0a9580f
thisissamridh <samridhsinghind@gmail.com>
2023-02-11 15:53:14 +0530

    even and odd arrays

--- Arrays/ReverseAlternateArray.cpp
+++ Arrays/ReverseAlternateArray.cpp
@@ -7,6 +7,7 @@ void printArray(int array[], int size)
     {
         cout << array[i] << " ";
     }
+    cout << endl;
 }
 
 void swapAlternate(int array[], int size)
@@ -22,10 +23,17 @@ void swapAlternate(int array[], int size)
 int main()
 {
 
-    int array[6] = {3, 1, 2, 7, 11, 8};
+    int array[6] = {3, 1, 2, 7, 11, 8}; // even
+    int odd[6] = {3, 2, 7, 11, 8};      // odd
+
+    cout << "printing the even array" << endl;
     printArray(array, 6);
     swapAlternate(array, 6);
     printArray(array, 6);
+    cout << "printing the odd array" << endl;
+    printArray(odd, 6);
+    swapAlternate(odd, 6);
+    printArray(odd, 6);
 
     return 0;
 }
